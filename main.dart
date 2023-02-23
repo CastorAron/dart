9e7b3407c059a34fed32c8b584192edeb3b6c09b
Castor Aron <castoraron255@gmail.com>
2023-02-23 15:13:49 +0300

    final

--- main.dart
+++ main.dart
@@ -1,21 +1,8 @@
 ///the entry point of my project.
 void main() {
-  int green = 0xFF00FF00;
-  int x = 0xF0;
-  int y = 0x0F;
-
-  print(x | y);
-  print((x | y).toRadixString(16));
-  print((x | y).toRadixString(2));
-
-  print(x & y);
-  print(x ^ y);
-  print(~y);
-  // ignore: todo
-  // TODO: #1 add me
-
-  //shift operaytor
-  int z = 4;
-  print((z >> 1).toRadixString(2));
-  print(z << 1);
+  final int x = 5;
+  var y = 10;
+  var string = "Hello";
+  print('x: $x' + ' y: $y $string');
+  x = 6;
 }
