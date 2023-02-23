2037e2f46fa9302aba47575de506e863093f8f0b
Castor Aron <castoraron255@gmail.com>
2023-02-23 12:19:22 +0300

    branch 10011

--- main.dart
+++ main.dart
@@ -1,18 +1,5 @@
 void main() {
-  double temperature = 20;
-  int value = 8;
-  String pizza = "pizza";
-  String pasta = "pasta";
-
-  print("$value plus $value makes ${value + value}" +
-      "\n" "I like " +
-      pizza +
-      " and " +
-      pasta);
-  print("Today I\'m feeling great !");
-  print("Today I'm feeling great !");
-  //raw string
-  print(r"My divider is \");
-  print(r"Today I'm feeling great !");
-  print(r"C:\windows\system32\drivers\etc");
+  String pizza = 'I love pizza';
+  print(pizza.contains('pizza'));
+  print(pizza.replaceAll('pizza', 'gamming'));
 }
