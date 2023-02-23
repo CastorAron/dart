83fa455e25604849f622b51c60f72a52c5310fe5
Castor Aron <castoraron255@gmail.com>
2023-02-23 12:53:12 +0300

    branch 1001122

--- main.dart
+++ main.dart
@@ -1,25 +1,15 @@
 void main() {
-  int age = 16;
-  String ageString = age.toString();
-  double height = 1.84;
-  String heightString = height.toString();
+  double tempFarenheight = 90;
 
-  print(ageString);
-  heightString = height.toStringAsFixed(1);
-  print(heightString);
-  String ratingString = '4.5';
-  double rating = double.parse(ratingString);
-  print(rating);
+  double tempCelcius;
+  tempCelcius = ((tempFarenheight - 32) / 1.8);
 
-  int x = 10;
-  double y = x.toDouble();
-  double z = 10;
+  // int C;
+  //C = tempCelcius.toInt();
 
-  int w = 40.678.round();
-  int c = 40.678.ceil();
-  int d = 40.678.floor();
-
-  print('$y $d $w $c ');
-  print(5 / 2);
-  print(5 ~/ 2);
+  print('${tempFarenheight.toStringAsFixed(0)}' +
+      'F ' +
+      '=' +
+      ' ${tempCelcius.toStringAsFixed(2)}' +
+      'C');
 }
