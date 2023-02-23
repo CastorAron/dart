5f8c24cd760d6a7778f6a9ce38420c4f6f7deaad
Castor Aron <castoraron255@gmail.com>
2023-02-23 12:40:04 +0300

    branch 100112

--- main.dart
+++ main.dart
@@ -1,5 +1,25 @@
 void main() {
-  String pizza = 'I love pizza';
-  print(pizza.contains('pizza'));
-  print(pizza.replaceAll('pizza', 'gamming'));
+  int age = 16;
+  String ageString = age.toString();
+  double height = 1.84;
+  String heightString = height.toString();
+
+  print(ageString);
+  heightString = height.toStringAsFixed(1);
+  print(heightString);
+  String ratingString = '4.5';
+  double rating = double.parse(ratingString);
+  print(rating);
+
+  int x = 10;
+  double y = x.toDouble();
+  double z = 10;
+
+  int w = 40.678.round();
+  int c = 40.678.ceil();
+  int d = 40.678.floor();
+
+  print('$y $d $w $c ');
+  print(5 / 2);
+  print(5 ~/ 2);
 }
