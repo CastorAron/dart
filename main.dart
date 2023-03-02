3c810a1078504827939cfd4cb1f9d7936c5b2e06
Castor Aron <castoraron255@gmail.com>
2023-03-02 12:35:03 +0300

    lists

--- main.dart
+++ main.dart
@@ -1,10 +1,24 @@
 void main() {
-  final cities = ['Arusha', 'Dar', 'Mbeya'];
-  // cities = ['Tokyo'];
-  cities[1] = 'Tokyo';
+  //sets only store unique values
 
-  const name = ['Alsice', 'Juma'];
-  // name = ['Tokyo'];
-  print(cities);
-  print(name);
+  var countries = {'Tanzania', 'Uganda', 'Kenya'};
+  print(countries.elementAt(0));
+  countries.add('Rwanda');
+  print(countries);
+  //it will add but ni change any thing
+  countries.add('Tanzania');
+  print(countries);
+
+  //how to remove item
+  countries.remove('Tanzania');
+  print(countries.first);
+  print(countries.last);
+  print(countries.contains('Rwanda'));
+  print(countries.length);
+  var euCountries = {'UK', 'German', 'Italy', 'Russia'};
+  var asiaCountries = {'China', 'Japan', 'Russia'};
+
+  print(euCountries.union(asiaCountries));
+  print(euCountries.intersection(asiaCountries));
+  print(euCountries.difference(asiaCountries));
 }
