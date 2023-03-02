17a34faf620710655b1de275e6c7705eb50f3c07
Castor Aron <castoraron255@gmail.com>
2023-03-02 15:00:25 +0300

    Nested collection

--- main.dart
+++ main.dart
@@ -1,37 +1,12 @@
 void main() {
-  const pizzaPrice = {
-    'margherita': 5.5,
-    'pepperon': 7.5,
-    'vegeterian': 6.5,
-  };
-
-  const order = [
-    'margherita',
-    'pepperon',
-    'pine',
-  ];
-  var total = 0.0;
-
-  for (var item in order) {
-    final price = pizzaPrice[item];
-    if (price != null) {
-      total += price;
-    } else {
-      print("$item pizza is not in the menu.");
+  var restaurant = [
+    {
+      'name': 'Pizza mario',
+      'cuisine': 'Italian',
+      'ratings': [5, 3.5, 14],
     }
+  ];
+  for (var i in restaurant) {
+    print(i);
   }
-
-  print("Total price is ${total}\$");
-
-  // const number = {
-  //   'number1': 12,
-  //   'number2': 14,
-  // };
-  // final numbers = ['number1', 'number2'];
-  // var innt = 0;
-  // for (var nth in numbers) {
-  //   final numm = number[nth];
-  //   innt += numm;
-
-  //     }
 }
